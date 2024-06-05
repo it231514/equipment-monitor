ea842da212e65dbef66293a0a7435e98a04edbf9
it231519 <it231519@fhstp.ac.at>
2024-06-05 14:43:59 +0200

    add goBack button in addEquipment Page

--- frontend/app/equipment/create/page.tsx
+++ frontend/app/equipment/create/page.tsx
@@ -72,125 +72,137 @@ function CreateEquipmentPage() {
 
   return (
     <div className="w-full min-h-[calc(100vh-64px)] back">
-      <div className="w-full h-full p-4 flex flex-row">
-        <div className="bg-white w-full h-full rounded-md overflow-hidden inline-flex flex-col flex-nowrap justify-center items-center">
-          <DateFnsProvider>
-            <div className="bg-[#fbdc00] shadow-md text-slate-700 font-bold text-lg w-full h-12 p-1 mb-2 flex justify-center items-center">
-              Assign Equipment
-            </div>
-            <form
-              onSubmit={handleSubmit(() => submit())}
-              noValidate
-              className="p-4 flex flex-col gap-2 w-full h-full text-black"
-            >
-              <Stack spacing={2}>
-                <div className="w-full flex justify-center items-center gap-x-3 bg-slate-100 rounded-md">
-                  <img className="max-h-52 rounded-md" src={image} />
-                </div>
-                <Button
-                  component="label"
-                  role={undefined}
-                  variant="contained"
-                  tabIndex={-1}
-                  startIcon={<CloudUploadIcon />}
-                  classes={{
-                    root: "hover:bg-[#fbdc00] hover:text-black text-white bg-slate-700 h-12",
-                  }}
-                >
-                  Upload Image
-                  <VisuallyHiddenInput
-                    type="file"
-                    onChange={handleUploadClick}
+      <div className="w-full h-full p-4 flex flex-col">
+        <Button
+          variant="contained"
+          color="error"
+          className="w-full rounded-md text-left mt-2"
+          onClick={() => {
+            router.push(`/equipment/list`);
+          }}
+        >
+          Abort without saving
+        </Button>
+        <div className="p-0 mt-4">
+          <div className="bg-white w-full h-full rounded-md overflow-hidden inline-flex flex-col flex-nowrap justify-center items-center">
+            <DateFnsProvider>
+              <div className="bg-[#fbdc00] shadow-md text-slate-700 font-bold text-lg w-full h-12 p-1 mb-2 flex justify-center items-center">
+                Assign Equipment
+              </div>
+              <form
+                onSubmit={handleSubmit(() => submit())}
+                noValidate
+                className="p-4 flex flex-col gap-2 w-full h-full text-black"
+              >
+                <Stack spacing={2}>
+                  <div className="w-full flex justify-center items-center gap-x-3 bg-slate-100 rounded-md">
+                    <img className="max-h-52 rounded-md" src={image} />
+                  </div>
+                  <Button
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                    classes={{
+                      root: "hover:bg-[#fbdc00] hover:text-black text-white bg-slate-700 h-12",
+                    }}
+                  >
+                    Upload Image
+                    <VisuallyHiddenInput
+                      type="file"
+                      onChange={handleUploadClick}
+                    />
+                  </Button>
+
+                  <TextFieldElement
+                    name={"articleNr"}
+                    label={"Article Number"}
+                    control={control}
+                    required
+                    fullWidth
                   />
-                </Button>
+                  <TextFieldElement
+                    name={"serialNr"}
+                    label={"Serial Number"}
+                    control={control}
+                    required
+                    fullWidth
+                  />
+                  <TextFieldElement
+                    name={"description"}
+                    label={"Description"}
+                    control={control}
+                    required
+                    fullWidth
+                  />
+                  <TextFieldElement
+                    name={"manufacturer"}
+                    label={"Manufacturer"}
+                    control={control}
+                    required
+                    fullWidth
+                  />
+                  <TextFieldElement
+                    name={"chairperson"}
+                    label={"Chairperson"}
+                    control={control}
+                    required
+                    fullWidth
+                  />
+                  <TextFieldElement
+                    name={"location"}
+                    label={"Location"}
+                    control={control}
+                    required
+                    fullWidth
+                  />
+                  <TextFieldElement
+                    name={"locationPrecise"}
+                    label={"Precise Location"}
+                    control={control}
+                    fullWidth
+                  />
+                  <DatePickerElement
+                    label={"Last Inspection Date"}
+                    name={"lastInspection"}
+                    control={control}
+                  />
+                  <DatePickerElement
+                    label={"Next Inspection Date"}
+                    name={"nextInspection"}
+                    control={control}
+                  />
 
-                <TextFieldElement
-                  name={"articleNr"}
-                  label={"Article Number"}
-                  control={control}
-                  required
-                  fullWidth
-                />
-                <TextFieldElement
-                  name={"serialNr"}
-                  label={"Serial Number"}
-                  control={control}
-                  required
-                  fullWidth
-                />
-                <TextFieldElement
-                  name={"description"}
-                  label={"Description"}
-                  control={control}
-                  required
-                  fullWidth
-                />
-                <TextFieldElement
-                  name={"manufacturer"}
-                  label={"Manufacturer"}
-                  control={control}
-                  required
-                  fullWidth
-                />
-                <TextFieldElement
-                  name={"chairperson"}
-                  label={"Chairperson"}
-                  control={control}
-                  required
-                  fullWidth
-                />
-                <TextFieldElement
-                  name={"location"}
-                  label={"Location"}
-                  control={control}
-                  required
-                  fullWidth
-                />
-                <TextFieldElement
-                  name={"locationPrecise"}
-                  label={"Precise Location"}
-                  control={control}
-                  fullWidth
-                />
-                <DatePickerElement
-                  label={"Last Inspection Date"}
-                  name={"lastInspection"}
-                  control={control}
-                />
-                <DatePickerElement
-                  label={"Next Inspection Date"}
-                  name={"nextInspection"}
-                  control={control}
-                />
-
-                <Autocomplete
-                  className="w-full"
-                  disablePortal
-                  multiple
-                  id="combo-box-demo"
-                  options={sensors?.map((sensor) => sensor.id) || []}
-                  getOptionLabel={(option) => option}
-                  sx={{ width: 300 }}
-                  onChange={(event, value) => {
-                    control._formValues.sensors = value;
-                  }}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Add Sensor" />
-                  )}
-                />
-                <br />
-                <Button
-                  type={"submit"}
-                  variant="contained"
-                  classes={{
-                    root: "hover:bg-[#fbdc00] hover:text-black text-white bg-slate-700 h-12",
-                  }}
-                >
-                  Create
-                </Button>
-              </Stack>
-            </form>
-          </DateFnsProvider>
+                  <Autocomplete
+                    className="w-full"
+                    disablePortal
+                    multiple
+                    id="combo-box-demo"
+                    options={sensors?.map((sensor) => sensor.id) || []}
+                    getOptionLabel={(option) => option}
+                    sx={{ width: 300 }}
+                    onChange={(event, value) => {
+                      control._formValues.sensors = value;
+                    }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Add Sensor" />
+                    )}
+                  />
+                  <br />
+                  <Button
+                    type={"submit"}
+                    variant="contained"
+                    classes={{
+                      root: "hover:bg-[#fbdc00] hover:text-black text-white bg-slate-700 h-12",
+                    }}
+                  >
+                    Create
+                  </Button>
+                </Stack>
+              </form>
+            </DateFnsProvider>
+          </div>
         </div>
       </div>
     </div>
